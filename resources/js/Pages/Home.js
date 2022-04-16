d1d7d84bf741c609361aaf07a790bc1d079deb44
easymagic <nnamware@yahoo.com>
2022-04-16 23:02:10 +0100

    np

---
+++ resources/js/Pages/Home.js
@@ -0,0 +1,6 @@
+const Home =({version='1.0.0'})=>{
+   return (<>
+     <div>Welcome to Inertia....{version}</div>
+   </>);
+};
+export default Home;
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return inertia()->render('Home',[
+        'version'=>'2.0.0'
+    ]);
 });
