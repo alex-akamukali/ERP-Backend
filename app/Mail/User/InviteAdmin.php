68ee92f6ca7c5ae3f3279b28eee781b48b5582ca
easymagic <nnamware@yahoo.com>
2022-04-24 17:07:46 +0100

    np

---
+++ app/Mail/User/InviteAdmin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail\User;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class InviteAdmin extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    //Mail::to($request->user())->send(new OrderShipped($order));
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+}
