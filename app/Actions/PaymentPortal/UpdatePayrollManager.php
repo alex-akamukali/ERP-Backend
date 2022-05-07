3b2d57f61a024a5ad43a9ceed8fb9d3657fdd1ff
easymagic <nnamware@yahoo.com>
2022-05-07 21:17:24 +0100

    np

---
+++ app/Actions/PaymentPortal/UpdatePayrollManager.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Actions\PaymentPortal;
+
+class UpdatePayrollManager
+{
+
+}
---
+++ app/Http/Controllers/PaymentPortal/AddPayrollManagerController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\PaymentPortal;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AddPayrollManagerController extends Controller
+{
+    //
+}
