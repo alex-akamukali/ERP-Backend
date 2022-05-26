5305e1060645b320e41d8d931a545c620ecebcf8
easymagic <nnamware@yahoo.com>
2022-05-26 15:46:32 +0100

    np

--- app/Http/Requests/Auth/UserManagementProfile/UpdateRequest.php
+++ app/Http/Requests/Auth/UserManagementProfile/UpdateRequest.php
@@ -24,7 +24,28 @@ class UpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            "first_name"=>"required",
+            "middle_name"=>"required",
+            "last_name"=>"required",
+            "gender"=>"required",
+            "highest_qualification"=>"required",
+            "dob"=>"required",
+            "phone"=>"required",
+            "program_type_id"=>"required",
+            "address"=>"required",
+            "province_id"=>"required",
+            "province_town_city_id"=>"required",
+            "postal_code"=>"required",
+            "account_status"=>"required",
+            "incorporation_name"=>"required",
+            "incorporation_address"=>"nullable",
+            "notes"=>"nullable",
+            "resume"=>"nullable",
+            "id_card"=>"nullable",
+            "void_check_doc"=>"nullable",
+            "hst_no"=>"required",
+            "assign_admin"=>"nullable",
+            "assign_director_id"=>"nullable",
         ];
     }
 
