255702d90c1c8868c1a027484f0855f0afb03564
easymagic <nnamware@yahoo.com>
2022-05-26 02:05:57 +0100

    np

--- database/migrations/2022_05_26_003149_add_field2_to_onboardings.php
+++ database/migrations/2022_05_26_003149_add_field2_to_onboardings.php
@@ -14,7 +14,7 @@ class AddField2ToOnboardings extends Migration
     public function up()
     {
         Schema::table('onboardings', function (Blueprint $table) {
-            $table->string('status')->nullable();
+            // $table->string('status')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddField2ToOnboardings extends Migration
     public function down()
     {
         Schema::table('onboardings', function (Blueprint $table) {
-            $table->dropColumn('status');
+            // $table->dropColumn('status');
         });
     }
 }
